bf46c3a809021d4171f3d03749be925bf65f632c
SimonGuerra007 <guerrasimon007@gmail.com>
2023-08-22 17:19:26 -0500

    errors fixed

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -28,7 +28,7 @@ const UsersList = ({
       {/* {currentUsers.length === 0 && <p>hola mundo</p> } */}
 
       <div className={`flex flex-col justify-center items-center text-[#AFAEAE]`}>
-        {currentUsers.length === 0 && <div className="bg-[#d9d9d9] rounded-full w-[150px] border-[5px] border-black mt-[100px]"><img src="../public/usuario.png" alt="" /></div>}
+        {currentUsers.length === 0 && <div className="bg-[#d9d9d9] rounded-full w-[150px] h-[150px] border-[5px] border-black mt-[100px]"><img src="../public/usuario.png" alt="" /></div>}
         {currentUsers.length === 0 && <h3 className="text-center">You don't have any users on your list.</h3>}
         {currentUsers.length === 0 && <h3>Create a new user</h3>}
       </div>
